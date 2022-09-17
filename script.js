5ef50d303047f33b3cbd531edf1257d90186d2c1
Glitch (glitch-hello-website) <none>
2022-09-17 13:59:45 +0000

    🌘🌂 Checkpoint ./script.js:695323/3501:48857213/80 ./index.html:695323/859:48857213/180

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
-'use strict';
+"use strict";
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-
-
-const image = document.querySelector('img');
+const image = document.querySelector("img");
 
 function drawCell(imageData, x, y, w, h) {
-    // get pixel colour
-    let index = (x + y * image.width) * 4;
-    let red = imageData[index + 0];
-    let green = imageData[index + 1];
-    let blue = imageData[index + 2];
+  // get pixel colour
+  let index = (x + y * image.width) * 4;
+  let red = imageData[index + 0];
+  let green = imageData[index + 1];
+  let blue = imageData[index + 2];
 
-    /// get brightness
-    let brightness = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
+  /// get brightness
+  let brightness = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
 
-    //
-    let new_width = (1.0 - (brightness / 255)) * w;
+  //
+  let new_width = (1.0 - brightness / 255) * w;
 
-    // fill background
-    // ctx.fillStyle = 'linear-gradient(blue, pink)';
-    ctx.fillStyle = 'black';
-    ctx.fillRect(x, y, w, h);
+  // fill background
+  // ctx.fillStyle = 'linear-gradient(blue, pink)';
+  ctx.fillStyle = "black";
+  ctx.fillRect(x, y, w, h);
 
-    ctx.fillStyle = `rgba(${red}, ${green}, ${blue}, 0.25)`;
-    ctx.fillRect(x, y, w, h);
+  ctx.fillStyle = `rgba(${red}, ${green}, ${blue}, 0.25)`;
+  ctx.fillRect(x, y, w, h);
 
-    //
-    let middle_x = x + (w / 2);
-    let new_x = middle_x - (new_width / 2);
-    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-    ctx.fillRect(
-        new_x, y, new_width, h - 1
-    );
+  //
+  let middle_x = x + w / 2;
+  let new_x = middle_x - new_width / 2;
+  ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+  ctx.fillRect(new_x, y, new_width, h - 1);
 }
 
-image.addEventListener('load', event => {
-  console.log('fyfan')
-    // draw image
-    ctx.drawImage(image, 0, 0, image.width, image.height);
+image.addEventListener("load", (event) => {
+  console.log("fyfan");
+  // draw image
+  ctx.drawImage(image, 0, 0, image.width, image.height);
 
-    //[r, g, b, a] 0-255
-    let imageData = ctx.getImageData(0, 0, image.width, image.height).data;
+  //[r, g, b, a] 0-255
+  let imageData = ctx.getImageData(0, 0, image.width, image.height).data;
 
-    // console.log(`value at pixel 0 is ${imageData[0]}`)
-    // console.log(`imagedata length: ${imageData.length}`)
+  // console.log(`value at pixel 0 is ${imageData[0]}`)
+  // console.log(`imagedata length: ${imageData.length}`)
 
-    // fill background
-    ctx.fillStyle = 'white';
-    // ctx.fillRect(0, 0, canvas.width, canvas.height)
+  // fill background
+  ctx.fillStyle = "white";
+  // ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    // create grid  
-    let grid_size_x = 12;
-    let grid_size_y = 16;
-    for (let y = 0; y < image.height; y += grid_size_y) {
-        for (let x = 0; x < image.width; x += grid_size_x) {
-            // draw cells
-            drawCell(imageData, x, y, grid_size_x, grid_size_y);
-        }
+  // create grid
+  let grid_size_x = 12;
+  let grid_size_y = 16;
+  for (let y = 0; y < image.height; y += grid_size_y) {
+    for (let x = 0; x < image.width; x += grid_size_x) {
+      // draw cells
+      drawCell(imageData, x, y, grid_size_x, grid_size_y);
     }
-
-
+  }
 });
